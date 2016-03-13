81c0a74da6f963d1adc984226e38b8b90d3a093d
Jethro Ordaneza <jet.oneza@gmail.com>
2016-03-13 20:36:06 +0800

    Check for APP_KEY in query

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -15,10 +15,18 @@ use App\Event;
  */
 class EventsController extends Controller
 {
-  public function index()
-  {
-    $events = Event::with('place')->with('violations')->get();
 
-    return $events;
+  const APP_KEY = 'oxotTRkIbWOvM90Bb9gp8UsTbMsEc2hm';
+
+  public function index(Request $request)
+  {
+    $key = $request->get('key');
+    if ($key === self::APP_KEY) {
+      $events = Event::with('place')->with('violations')->get();
+
+      return $events;
+    } else {
+      return ['error' => 'Your query is not authorized!'];
+    }
   }
 }
