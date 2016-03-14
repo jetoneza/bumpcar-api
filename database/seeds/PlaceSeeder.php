51e1818a631a2c32c5d30812a16dac2d6f23fe81
Jethro Ordaneza <jet.oneza@gmail.com>
2016-03-14 21:19:20 +0800

    Added additional places to seeder

--- database/seeds/PlaceSeeder.php
+++ database/seeds/PlaceSeeder.php
@@ -41,5 +41,35 @@ class PlaceSeeder extends Seeder
     $place->latitude  = '7.059725';
     $place->longitude = '125.551060';
     $place->save();
+
+    $place            = new Place();
+    $place->name      = 'Quimpo Blvd cor Tulip Dr';
+    $place->latitude  = '7.0517243';
+    $place->longitude = '125.59001684';
+    $place->save();
+
+    $place            = new Place();
+    $place->name      = 'JP Laurel ave. Flyover';
+    $place->latitude  = '7.09550548';
+    $place->longitude = '125.61598063';
+    $place->save();
+
+    $place            = new Place();
+    $place->name      = 'Milan Crossing';
+    $place->latitude  = '7.10870717';
+    $place->longitude = '125.61336279';
+    $place->save();
+
+    $place            = new Place();
+    $place->name      = 'Francisco Bangoy International Airport Terminal';
+    $place->latitude  = '7.13374673';
+    $place->longitude = '125.64404726';
+    $place->save();
+
+    $place            = new Place();
+    $place->name      = 'McArthur Hwy cor Don Julian R. Sr. Ave.';
+    $place->latitude  = '7.06180754';
+    $place->longitude = '125.59293509';
+    $place->save();
   }
 }
